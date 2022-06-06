51181ee2b0ba5ee661c88503e785777f06d66a57
Matias Froiman <93104082+narucho89@users.noreply.github.com>
2022-06-06 19:40:17 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './Components/NavBar/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+import ItemCount2 from './Components/ItemCount/ItemCount2';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     
+    return (
+        <>
+            <NavBar />
+            <ItemListContainer greeting='Bienvenido a Narutech' />
+            <ItemCount2 stock={10} initial={1} /> 
+
+
+        </>
+    )
 }
 
-export default App;
+export default App
