6069a18153bb005c26442a63f50f1fb076281fc7
Matias Froiman <93104082+narucho89@users.noreply.github.com>
2022-06-24 00:04:30 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,24 +3,25 @@ import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Cart from './Components/Cart/Cart'
-
-
+import { CartContext } from './Components/Context/cartContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
-     
+
     return (
-       <BrowserRouter>
-            <NavBar />
-            <Routes>
-                <Route index  path='/' element={<ItemListContainer greeting='Bienvenido a Narutech' />}/>
-                <Route path='/categorias/:categoria' element={<ItemListContainer/>}/>
-                <Route path='/detalle/:id' element={<ItemDetailContainer/>}/>
-                <Route path='*' element={<Navigate to='/'/>}/>
-                <Route path='/Cart' element={<Cart/>}/>
-            </Routes>
-       </BrowserRouter>
+        <CartContext.Provider>
+            <BrowserRouter>
+                <NavBar />
+                <Routes>
+                    <Route index path='/' element={<ItemListContainer greeting='Bienvenido a Narutech' />} />
+                    <Route path='/categorias/:categoria' element={<ItemListContainer />} />
+                    <Route path='/detalle/:id' element={<ItemDetailContainer />} />
+                    <Route path='*' element={<Navigate to='/' />} />
+                    <Route path='/Cart' element={<Cart />} />
+                </Routes>
+            </BrowserRouter>
+        </CartContext.Provider>
     )
 }
 
